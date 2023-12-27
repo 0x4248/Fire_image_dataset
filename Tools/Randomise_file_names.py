c6c5c708522644533ef1149f97f251b3ac3da21a
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2023-12-27 21:14:03 +0000

    Update tool
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

--- Tools/Randomise_file_names.py
+++ Tools/Randomise_file_names.py
@@ -4,4 +4,4 @@ import os
 set_number = "Images/Set "+input("Set Number: ")
 
 for file in os.listdir(set_number):
-    os.rename(folder + "/" + file, folder + "/" + str(random.randint(0, 100000000)) + ".jpg")
+    os.rename(set_number + "/" + file, set_number + "/" + str(random.randint(0, 100000000)) + ".jpg")
